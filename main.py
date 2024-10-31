306cb8fa33f9115df56cc55e67c67b0e9931aa76
Sofia313131 <sofiatataurova90283@gmail.com>
2024-10-31 20:22:51 +0300

    Realise 1.0 (All task solved)

--- main.py
+++ main.py
@@ -1,59 +1,176 @@
-import tkinter as tk
-from tkinter import messagebox
-from sqrEq import sqrEquation
+import os.path
+import sys
+from os import environ
+from random import shuffle, choice
+from string import ascii_uppercase
+
+if "linux" in sys.platform:
+    import pyperclip
+else:
+    import win32clipboard
+
+# Скрываем приветствие от PyGame
+environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
 
-def close():
-    window.destroy()
+import pygame
+from pygame import Surface, Rect
+
+NUMBER_OF_NUMBERS = 6
+NUMBER_START_POINT = (250, 100)
+OUTPUT_FIELD_COORDS = (120, 500)
+NUMBER_DELTA = 100
+DARK_GREEN = (0, 170, 0)
+DARK_GRAY = (50, 50, 50)
+GRAY = (150, 150, 150)
+FPS = 60
 
 
-def calc():
-    A = float(arg_A.get())
-    B = float(arg_B.get())
-    C = float(arg_C.get())
-    if A == 0.0:
-        tk.messagebox.showwarning('Error', 'Division by zero!')
+def copy(text):
+    if "linux" in sys.platform:
+        pyperclip.copy(text)
     else:
-        lbl_result.configure(text=sqrEquation(A, B, C))
+        win32clipboard.OpenClipboard()
+        win32clipboard.EmptyClipboard()
+        win32clipboard.SetClipboardText(text, win32clipboard.CF_UNICODETEXT)
+        win32clipboard.CloseClipboard()
 
 
-window = tk.Tk()
-window.geometry('576x360')
-bg_img = tk.PhotoImage(file='bg_pic.png')
+def keygen(s):
+    if " " in s:
+        return ""
+    n1, n2 = list(s[:3]), list(s[3:])
+    shuffle(n1)
+    shuffle(n2)
+    n1, n2 = "".join(n1), "".join(n2)
 
-lbl_bg = tk.Label(window, image=bg_img)
-lbl_bg.place(x=0, y=0, relwidth=1, relheight=1)
+    ls = [choice(ascii_uppercase) for _ in range(4)]
+    sm = str(int(n1)+int(n2)).rjust(4, "0")
 
-frame = tk.Frame(window)
-frame.place(relx=0.5, rely=0.5, anchor='center')
-
-lbl_A = tk.Label(frame, text='A', font=('Arial', 30), bg='blue', fg='white')
-lbl_A.grid(column=0, row=0, padx=10, pady=15)
-arg_A = tk.Entry(frame, width=10)
-arg_A.insert(0, '1')
-arg_A.grid(column=0, row=1, padx=10, pady=15)
-
-lbl_B = tk.Label(frame, text='B', font=('Arial', 30))
-lbl_B.grid(column=1, row=0, padx=10, pady=15)
-arg_B = tk.Entry(frame, width=10)
-arg_B.insert(0, '0')
-arg_B.grid(column=1, row=1, padx=10, pady=15)
-
-lbl_C = tk.Label(frame, text='C', font=('Arial', 30))
-lbl_C.grid(column=2, row=0, padx=10, pady=15)
-arg_C = tk.Entry(frame, width=10)
-arg_C.insert(0, '0')
-arg_C.grid(column=2, row=1, padx=10, pady=15)
-
-lbl_roots = tk.Label(frame, text='Result:')
-lbl_roots.grid(column=1, row=2)
-lbl_result = tk.Label(frame, text='None yet.', font=('Arial', 10))
-lbl_result.grid(column=2, row=2)
-
-btn_calc = tk.Button(frame, text='Calculate', command=calc)
-btn_calc.grid(column=0, row=3, padx=10, pady=15)
-btn_exit = tk.Button(frame, text='Cancel', command=close)
-btn_exit.grid(column=2, row=3, padx=10, pady=15)
+    return f"{n1}{ls[0]}{ls[1]}-{n2}{ls[2]}{ls[3]}-{sm}"
 
 
-window.mainloop()
+class NumberContainer:
+    def __init__(self, x, y, next_node=None, prev_node=None):
+        image = pygame.image.load(os.path.join(os.curdir, "green_rectangle.png")).convert_alpha()
+        image = pygame.transform.rotate(image, 180)
+        image = pygame.transform.scale(image, (80, 100))
+
+        self.image = image
+        self.font = pygame.font.SysFont('Comic Sans MS', 140)
+        self.text = " "
+        self.enable = False
+        self.tick = 0
+
+        self.x = x
+        self.y = y
+
+        self.next = next_node
+        self.prev = prev_node
+
+    def write(self, event):
+        if event.key == pygame.K_LEFT:
+            if self.prev is not None:
+                self.enable = False
+                self.prev.enable = True
+        elif event.key == pygame.K_RIGHT:
+            if self.next is not None:
+                self.enable = False
+                self.next.enable = True
+        key = event.unicode
+        if key.isdigit() and len(key) == 1:
+            self.text = key
+            if self.next is not None:
+                self.enable = False
+                self.next.enable = True
+        elif key.isspace() and key not in "\n\r":
+            self.text = " "
+        elif key == "\b":
+            self.text = " "
+            if self.prev is not None:
+                self.enable = False
+                self.prev.enable = True
+
+    def draw(self, screen: Surface):
+        self.tick = (self.tick + 1) % 60
+        screen.blit(self.image, (self.x, self.y))
+
+        text = self.font.render(self.text, False, DARK_GREEN)
+        screen.blit(text, (self.x + 15, self.y + 10))
+
+        if self.enable and self.tick < 30:
+            pygame.draw.line(screen, DARK_GREEN, (self.x + 15, self.y + 85), (self.x + 65, self.y + 85), 5)
+
+
+def main():
+    pygame.init()
+
+    pygame.display.set_caption("KeyGen v1.0")
+    screen = pygame.display.set_mode((1366, 768))
+
+    clock = pygame.time.Clock()
+
+    background = pygame.image.load(os.path.join(os.curdir, "bg_pic.png"))
+    background_rect = background.get_rect()
+
+    darkness = pygame.Surface((1366, 768), pygame.SRCALPHA)
+    darkness_rect = darkness.get_rect()
+
+    darkness.set_alpha(128)
+    darkness.fill((0, 0, 0))
+
+    copy = pygame.image.load(os.path.join(os.curdir, "copy.png"))
+    copy = pygame.transform.scale(copy, (80, 80))
+    font = pygame.font.SysFont('Comic Sans MS', 140)
+    copy_rect = Rect(OUTPUT_FIELD_COORDS[0] + 1010, OUTPUT_FIELD_COORDS[1] + 10, 80, 80)
+
+    containers = []
+    for i in range(1, NUMBER_OF_NUMBERS + 1):
+        containers.append(NumberContainer(
+            NUMBER_START_POINT[0] + NUMBER_DELTA * i,
+            NUMBER_START_POINT[1]
+        ))
+    for i in range(1, NUMBER_OF_NUMBERS):
+        containers[i].prev = containers[i - 1]
+    for i in range(NUMBER_OF_NUMBERS - 1):
+        containers[i].next = containers[i + 1]
+
+    containers[0].enable = True
+
+    answer = ""
+
+    while True:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                for container in containers:
+                    if container.enable:
+                        container.write(event)
+                        break
+                if event.key == pygame.K_RETURN:
+                    answer = keygen("".join([i.text for i in containers]))
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    # Call the on_mouse_button_down() function
+                    if copy_rect.collidepoint(event.pos):
+                        copy(answer)
+
+        screen.blit(background, background_rect)
+        screen.blit(darkness, darkness_rect)
+
+        for container in containers:
+            container.draw(screen)
+
+        pygame.draw.rect(screen, GRAY, (*OUTPUT_FIELD_COORDS, 1100, 100))
+        text = font.render(answer, False, DARK_GRAY)
+        screen.blit(text, (OUTPUT_FIELD_COORDS[0] + 10, OUTPUT_FIELD_COORDS[1] + 10))
+        screen.blit(copy, (OUTPUT_FIELD_COORDS[0] + 1010, OUTPUT_FIELD_COORDS[1] + 10))
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    main()
